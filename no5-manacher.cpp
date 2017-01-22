6cea20edc0b2aaaea28e17499f1c8c0d16419f08
EnchelQ <335747282@qq.com>
2017-01-22 10:54:05 +0800

    Solution for Longest Palindrome String problem

--- no5-manacher.cpp
+++ no5-manacher.cpp
@@ -1,11 +1,38 @@
+/*
+manacher算法，用来求解最长回文字符串问题。
+时间复杂度为O(n)
+算法思路：
+	对原字符串预处理，在其中插入字符‘#’，问题变成只需要求extends字符串以每个字符为对称中心的回文字符串大小。
+	对每一个extends元素求出其对应的assist数组的值，并返回最大的assist值。
+		assist值的大小为从中心元素开始，到以他为中心的回文字符串的右边边界位置，字符的个数。
+		assist-1的正好是对原字符串s对应的回文字符串大小。
+	由extends中longest的位置来确定原字符串对应的回文字符串位置。
+*/
 #include<iostream>
 #include<string>
 #include<vector>
 using namespace std;
+int getPalinLen(vector<int> &, string);
+string longestPalindrome(string s)
+{
+	string extends;
+	int k = 0;
+	for (int i = 0; i < s.size(); i++)
+	{
+		extends += '#';
+		extends += s[i];
+	}
+	extends += '#';
+	vector<int> assist;
+	int longest = getPalinLen(assist, extends);
+	int len = assist[longest];
+	return s.substr((longest - len + 1) / 2, len - 1);
+}
+
 //对每一个extends元素求出其对应的assist数组的值，并返回最大的assist值。
 int getPalinLen(vector<int> &assist, string extends)
 {
-	int mx=0;//mx用来指示已访问的字符中，拥有最右回文字符串的字符编号编号。
+	int mx = 0;//mx用来指示已访问的字符中，拥有最右回文字符串的字符编号编号。
 	int longest = 0;//longest用来指示已访问字符中，拥有最大回文字符串长度的字符编号。
 	assist.push_back(1);//第一个元素回文字符串大小永远为一。
 	for (int i = 1; i < extends.size(); i++)
@@ -14,7 +41,7 @@ int getPalinLen(vector<int> &assist, string extends)
 		//step2:判断i关于拥有最右边界的元素mx对称的元素的回文字符串有没有越界。
 		int ppos, npos;
 		if (assist[i - 1] > assist[longest])//还用在循环外写对最后一个元素的访问情况吗？不用。
-			longest =i-1;
+			longest = i - 1;
 		int mxn = mx + assist[mx] - 1;//mxn用来指示最右边界。
 		if (i < mxn)
 		{
@@ -28,7 +55,7 @@ int getPalinLen(vector<int> &assist, string extends)
 			{
 				assist.push_back(assist[j]);
 				continue;
-			}				
+			}
 			else if (in>mxn)
 			{
 				assist.push_back(mxn - i + 1);
@@ -36,15 +63,15 @@ int getPalinLen(vector<int> &assist, string extends)
 			}
 			else
 			{
-				 ppos = i - assist[j];
-				 npos = mxn + 1;
-				
+				ppos = i - assist[j];
+				npos = mxn + 1;
+
 			}
 		}
 		else
 		{
-			 ppos = i - 1;
-			 npos = i + 1;
+			ppos = i - 1;
+			npos = i + 1;
 		}
 
 		while (ppos >= 0 && npos<extends.size() && extends[ppos] == extends[npos])
@@ -53,37 +80,14 @@ int getPalinLen(vector<int> &assist, string extends)
 			npos++;
 		}
 		mx = i;
-		assist.push_back( npos - i);
+		assist.push_back(npos - i);
 	}
 	return longest;
 }
 
-string longestPalindrome(string s)
-{
-	//对原字符串预处理，在其中插入字符‘#’，问题变成只需要求extends字符串以每个字符为对称中心的回文字符串大小。
-	string extends;
-	int k = 0;
-	for (int i = 0; i < s.size(); i++)
-	{
-		extends += '#';
-		extends += s[i];
-	}
-	extends += '#';
-	//assist值的大小为从中心元素开始，到以他为中心的回文字符串的右边边界位置，字符的个数。
-	//assist-1的正好是对原字符串s对应的回文字符串大小。
-	vector<int> assist;
-	int longest = getPalinLen(assist, extends);
-	//由extends中longest的位置来确定原字符串对应的回文字符串位置。
-	int len = assist[longest];
-	int ppos = (longest - len + 1)/2;
-	int npos = (longest + len - 3)/2;
-	string result;
-	for (int i = ppos; i <= npos;i++)
-		result += s[i];
-	return result;
-}
 int main()
 {
 	string s = "skjehiuihehiuihej";
+
 	cout << longestPalindrome(s);
 }
--- no5.cpp
+++ no5.cpp
@@ -2,8 +2,8 @@
 问题描述：求解最长回文字符串。
 解决方案描述：
 	step1:循环遍历输入字符串中的每个字符。
-	step2:求以当前字符为对称中心的奇数回文字符串和以当前字符为对称轴左侧字符的偶数字符串，取最长的那个字符串。
-	step3:将当前字符串对应的最大字符串与全局最大字符串比较，将最大的存入全局最大字符串。
+	step2:求以当前字符为对称中心的奇数回文字符串和以当前字符为对称轴左侧字符的偶数字符串，记录最长的回文字符串长度及中心位置。
+	step3:利用最长的回文字符串长度及中心位置确定最长回文字符串
 解决方案时间复杂度O(n^2).
 */
 #include<iostream>
@@ -11,16 +11,16 @@
 using namespace std;
 string longestPalindrome(string s)
 {
-
 	int len = s.size();
 	if (len == 1)
 		return s;
+		//return s;
 	int pos = 0;
-	string longestPal;
+	int longest = 0, maxLong = 0;//longest用来指示拥有最大回文字符串的中心字符位置，maxLong用来指示最大回文字符串长度。
 	//step1:循环遍历输入字符串中的每个字符。
 	while (pos < len)
 	{
-		//step2:求以当前字符为对称中心的奇数回文字符串和以当前字符为对称轴左侧字符的偶数字符串，取最长的那个字符串。
+//	step2:求以当前字符为对称中心的奇数回文字符串和以当前字符为对称轴左侧字符的偶数字符串，记录最长的回文字符串长度及中心位置。
 		for (int i = -1; i < 1; i++)
 		{
 			int ppos = pos + i;//当i=-1和i=0的时候对应两种情况
@@ -35,31 +35,22 @@ string longestPalindrome(string s)
 				else
 					break;
 			}
-			ppos++;
-			//如果当前字符的最大回文字符串大于最大回文字符串，则重新给最大回文字符串longestPal赋值。
-			if ((npos - ppos)>longestPal.size())
+			if ((npos - ppos-1)>maxLong)
 			{
-				int k = 0;
-				int j = ppos;
-				for(; j <ppos+longestPal.size();j++)
-				{
-					longestPal[k] = s[j];
-					k++;
-				}
-				for (; j < npos; j++)
-				{
-					longestPal += s[j];
-					k++;
-				}
+				longest = pos;
+				maxLong = npos - ppos-1;
 			}
 		}
 		pos++;
 	}
-	return longestPal;
+	if (maxLong % 2)
+		return s.substr(longest - maxLong / 2, maxLong);
+	else
+		return s.substr(longest - maxLong / 2 + 1, maxLong);
 }
 
 int main()
 {
-	string s = "ccc";
+	string s = "skjehiuihehiuihej";
 	cout << longestPalindrome(s);
 }
